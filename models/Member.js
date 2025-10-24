c479331914863410e5161c7b66df45e57fbee3ca
lucas9944 <xolmirzayevotabek779@gmail.com>
2025-10-24 16:49:57 +0900

    feat: hashing passwords based on the bcrypt package

--- models/Member.js
+++ models/Member.js
@@ -1,7 +1,7 @@
-
 const MemberModel = require("../schema/member.model");
 const Definer = require("../lib/mistake");
 const assert = require("assert");
+const bcrypt = require("bcryptjs");
 
 class Member {
   constructor() {
@@ -10,11 +10,12 @@ class Member {
 
   async signupData(input) {
     try {
+      const salt = await bcrypt.genSalt();
+      input.mb_password = await bcrypt.hash(input.mb_password, salt);
       const new_member = this.memberModel(input);
       const result = await new_member.save();
 
       try {
-        
       } catch (mongo_err) {
         console.log(mongo_err);
         throw new Error(Definer.auth_err1);
@@ -33,7 +34,10 @@ class Member {
         .exec();
 
       assert.ok(member, Definer.auth_err2);
-      const isMatch = input.mb_password === member.mb_password;
+      const isMatch = await bcrypt.compare(
+        input.mb_password,
+        member.mb_password
+      );
       assert.ok(isMatch, Definer.auth_err3);
 
       return await this.memberModel.findOne({ mb_nick: input.mb_nick }).exec();
