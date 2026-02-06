89db940e7e8b23f097c98e796e99007a10234d0d
Lucas9944 <xolmirzayevotabek779@gmail.com>
2026-02-06 23:00:17 +0900

    feat: develop getMemberArticles rest api logic part two done

--- router.js
+++ router.js
@@ -82,4 +82,12 @@ router.post(
   communityController.createArticle
 );
 
+router.get(
+  "/community/articles",
+  memberController.retrieveAuthmember,
+  communityController.getMemberArticles
+);
+/*******************************
+ *        END REST API         *
+ *******************************/
 module.exports = router;
